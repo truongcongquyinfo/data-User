7723d2973987140ff33beba9490dc330c3483c7b
Trương Công Quý ★ ☭ ♚ ♫ ✓ <155416036+truongcongquyinfo@users.noreply.github.com>
2024-10-22 06:44:07 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
 
 server.use(middlewares);
+server.use((req, res, next) => {
+  if (req.method === 'POST') {
+    res.status(403).send('POST method is not allowed')
+  } else {
+    next()
+  }
+})
+
 server.use(router);
 
 server.listen(port);
